2778908370fced54b13ad0e5f32045487ad43d0e
mathew4STAR <mathewph249@gmail.com>
2022-01-18 18:45:18 +0530

    testing folder

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+from ctypes import alignment
 import tkinter as tk
+from turtle import color
 
 
 print("welcome to setup")
 
-root = tk.TK()
+root = tk.Tk()
+
+root.size((1920, 1080))
 
 label = tk.Label(root, text="hello welcome to the setup")
+label.pack()
+
+start = tk.Button(root, text= "START SETUP", anchor="center")
+start.pack()
+
+root.mainloop()
