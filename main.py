488bb221ebd241a331b8c30e633f01ca68419a6e
mathew4STAR <mathewph249@gmail.com>
2022-01-19 11:27:45 +0530

    added few changes

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def addclass(classs, div, data):
     f.write(data)
     f.close()
 
-def check_attendance(configuration):
-    time.sleep(int(configuration[0]))
+def check_attendance(configuration, target):
+    #time.sleep(int(configuration[0]))
     strength = int(configuration[1])
     floc = configuration[2].split()
     sloc = configuration[3].split()
@@ -25,7 +25,7 @@ def check_attendance(configuration):
     pg.click(firstloc)
     pg.moveTo(secondloc)
     absent = []
-    participants = open("data//kids.txt")
+    participants = open("data//" + target + ".txt")
     for i in participants:
         i = i.strip("\n")
         pg.write(i)
@@ -40,17 +40,33 @@ def check_attendance(configuration):
     print("ABSENT:", len(absent))
     print("Absentees")
     print(absent)
-    input('Press enter to close the application')
+    final = ""
+    for i in absent:
+        final = final + i + " "
+    children.insert(tk.END, final)
+    
 
 root = tk.Tk()
-label = tk.Label(root, text="hello welcome to this program")
-label.pack()
+intro = tk.Label(root, text="hello welcome to this program")
+intro.pack()
 config = open("data//config.txt")
 configuration = []
 for i in config:
     configuration.append(i.strip("\n"))
 
-button = tk.Button(root, text= "START PROCESS", command= lambda: check_attendance(configuration))
-button.pack()
-
+check_class = tk.Entry(root)
+check_class.insert(0, "Please enter the class:")
+check_class.pack()
+attenddence_initiate = tk.Button(root, text= "START PROCESS", command= lambda: check_attendance(configuration, check_class.get()))
+attenddence_initiate.pack()
+theclass = tk.Entry(root)
+theclass.insert(0, "Please enter the class if you want to add a new class to the database")
+thesection = tk.Entry(root)
+thesection.insert(0, "Please enter the section if you want to add a new class to the database")
+children = tk.Text(root, height=20, width=100)
+theclass.pack()
+thesection.pack()
+children.pack()
+classadder = tk.Button(root, text= "Add a class", command= lambda: addclass(theclass.get(), thesection.get(), children.get("1.0", "end-1c")))
+classadder.pack()
 root.mainloop()
