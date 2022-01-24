ea0e1277f9469bdfa870c6842a0d1fa5bbcc6568
mathew4STAR <mathewph249@gmail.com>
2022-01-24 12:10:19 +0530

    completed project mark 1

--- main.py
+++ main.py
@@ -18,32 +18,13 @@ def gotoscene1():
     scene2.forget()
     scene1.pack(fill="both", expand = True)
 
-class LineNumbers(tk.Text):
-    def __init__(self, master, text_widget, **kwargs):
-        super().__init__(master, **kwargs)
-
-        self.text_widget = text_widget
-        self.text_widget.bind('<KeyPress>', self.on_key_press)
-
-        self.insert(1.0, '1')
-        self.configure(state='disabled')
-
-    def on_key_press(self, event=None):
-        final_index = str(self.text_widget.index(tk.END))
-        num_of_lines = final_index.split('.')[0]
-        line_numbers_string = "\n".join(str(no + 1) for no in range(int(num_of_lines)))
-        width = len(str(num_of_lines))
-
-        self.configure(state='normal', width=width)
-        self.delete(1.0, tk.END)
-        self.insert(1.0, line_numbers_string)
-        self.configure(state='disabled')
-
 
 def addclass(classs, div, data):
-    f = open(("data//classes" + classs + div + ".txt"), "w+")
+    f = open(("data//classes//" + classs + div + ".txt"), "w+")
     f.write(data)
     f.close()
+    newclassadd = open("data//classes.txt", "a")
+    newclassadd.write("\n" + classs + div)
 
 def check_attendance(configuration, target):
     time.sleep(int(configuration[0]))
@@ -65,11 +46,6 @@ def check_attendance(configuration, target):
             absent.append(i)
         pg.hotkey('ctrl', 'shift', 'backspace')
         
-    print("STRENCTH:", strength)
-    print("PRESENT:", strength - len(absent))
-    print("ABSENT:", len(absent))
-    print("Absentees")
-    print(absent)
     final = ""
     thebox.delete("1.0","end")
     for i in absent:
@@ -82,6 +58,7 @@ root = tk.Tk()
 root.geometry("960x540")
 root.minsize(960, 540)
 root.maxsize(960, 540)
+root.title("Attendance bot")
 bg = tk.PhotoImage(file = "data//image3.png")
 scene1 = tk.Canvas(root)
 scene1.pack(fill="both", expand = True)
@@ -100,9 +77,7 @@ attenddence_initiate = tk.Button(scene1, image= attendance_btn_img, borderwidth=
 
 
 scene2 = tk.Frame(root)
-scene2_2 = tk.Frame(scene2)
 scene2.pack(fill = "both", expand = True)
-scene2_2.pack(fill="both", expand = True)
 scene_2_button = tk.Button(scene1, text = "Edit classes list", command = gotoscene2)
 scene_2_button.place(x = 840, y = 24)
 
@@ -113,35 +88,28 @@ newlabel = tk.Label(scene2, text="Add a class", bg= "#2D8CFF", fg = "white", hei
 newlabel.pack()
 
 classlabel = tk.Label(scene2, text = "Class: ")
-classlabel.pack(anchor=tk.W, pady = (30, 0))
-#classlabel.place(x = 270, y = 230)
-
+classlabel.place(x = 70, y = 120)
 
 classentry = tk.Entry(scene2)
-#classentry.place(x = 300, y = 230)
-classentry.pack(anchor=tk.W)
+classentry.place(x = 120, y = 120)
 
-sectionlabel = tk.Label(scene2_2, text = "Section: ")
-sectionlabel.pack(anchor=tk.W, pady = (10, 0))
-#classlabel.place(x = 270, y = 230)
+sectionlabel = tk.Label(scene2, text = "Section: ")
+#sectionlabel.pack(anchor=tk.W, pady = (10, 0))
+sectionlabel.place(x = 70, y= 150)
 
+sectionentry = tk.Entry(scene2)
+sectionentry.place(x= 130, y= 150)
 
-sectionentry = tk.Entry(scene2_2)
-#classentry.place(x = 300, y = 230)
-sectionentry.pack(anchor=tk.W)
+children = tk.Text(scene2, height=10, width=70)
+children.insert(tk.END, "Please Enter the children of the respective class here")
+children.place(x = 130, y = 215)
 
-sectionlabel.grid(row=0, column=0)
-sectionentry.grid(row=0, column=1)
-
-children = tk.Text(scene2, height=10, width=100)
-children.pack()
-
-classadder = tk.Button(scene2, text= "Add a class", command= lambda: addclass(classentry.get(), sectionentry.get(), children.get("1.0", "end-1c")))
-classadder.pack()
+classadder = tk.Button(scene2, text= "Add this class", command= lambda: addclass(classentry.get(), sectionentry.get(), children.get("1.0", "end-1c")))
+classadder.place(x = 420, y = 450)
 
 scene2.forget()
 clicked = tk.StringVar()
-clicked.set("10A")
+clicked.set("T1")
 check_class = tk.OptionMenu(scene1, clicked, *classess)
 check_class.config(width=1, height = 2, bg="#2D8CFF", fg="white")
 check_class.place(x = 270, y = 230)
@@ -150,20 +118,6 @@ thebox = tk.Text(scene1, height = 8, width = 27 )
 thebox.insert(tk.END, "Attendance not calculated yet.")
 thebox.bind("<Key>", lambda e: "break")
 thebox.place(x = 542, y = 290)
-#check_class.insert(0, "Please enter the class:")
 
-
-
-#theclass = tk.Entry(scene1)
-#theclass.insert(0, "Please enter the class if you want to add a new class to the database")
-#thesection = tk.Entry(scene1)
-#thesection.insert(0, "Please enter the section if you want to add a new class to the database")
-#children = tk.Text(scene1, height=20, width=100)
-#theclass.pack()
-#thesection.pack()
-#children.pack(expand= 1)
-
-#classadder = tk.Button(root, text= "Add a class", command= lambda: addclass(theclass.get(), thesection.get(), children.get("1.0", "end-1c")))
-#classadder.pack()
 
 root.mainloop()
