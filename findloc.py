d61a4f7bc6bcdccf629209ddc151869f280fa810
mathew4STAR <mathewph249@gmail.com>
2022-02-03 16:21:15 +0530

    added findloc again

---
+++ findloc.py
@@ -0,0 +1,13 @@
+import time 
+import pyautogui as pag
+
+#change the buffer time in case you need more, or less
+buffer = 5 
+
+time.sleep(buffer)
+
+locx, locy = pag.position()
+
+print(str(locx) + ' ' + str(locy))
+
+input("Press Enter to close")
